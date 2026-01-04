ad516e5383885f1efd643d125c71b8e605bfc188
Samiii <samibhadgaonkar@gmail.com>
2026-01-05 01:08:10 +0530

    updated the new routes

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -5,6 +5,10 @@ const { createUser,
     getUserProfile,
     updateUser,
     updateWorkoutStatus,
+    startWorkoutV2,
+    getWorkoutProgress,
+    workoutCompleteDayExercise,
+    workoutCompleteDay,
     startWorkout } = require('../controllers/User.controller')
 const FileUpload = require('../middlewares/multer')
 const { handleProfileUpload } = require('../controllers/Cloudinary.controller')
@@ -14,8 +18,11 @@ const router = express.Router()
 router.post('/login', loginUser)
 router.post('/signup', createUser)
 router.post('/update', protect, updateUser)
-router.post('/enroll', protect, startWorkout)
+router.post('/enroll', protect, startWorkoutV2)
 router.post('/updateProgress', protect, updateWorkoutStatus)
+router.get('/workouts/:id/progress', protect, getWorkoutProgress)
+router.post('/workouts/:id/day/:n/exercise/:x/complete', protect, workoutCompleteDayExercise)
+router.post('/workouts/:id/day/:n/complete', protect, workoutCompleteDay)
 router.post('/upload', (req, res, next) => {
     if (req.body.secret === process.env.UPLOAD_SECRET) {
         next()
